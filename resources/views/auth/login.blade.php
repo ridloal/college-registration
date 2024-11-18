e0e8759080b8027bea87312b8716e86451221e50
Ridlo Alimudin <ridlo.alimudin@gmail.com>
2024-11-18 10:10:13 +0700

    feat: modif login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,22 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        <!-- add notes for default admin and password -->
+        <div class="mt-10">
+            <hr>
+            <h2 style="text-align: center;">NOTES</h2>
+            <hr>
+            <p class="text-sm text-gray-600 items-center mt-5">Default admin email: <b>admin@admin.com</b></p>
+            <p class="text-sm text-gray-600 items-center">Default admin password: <b>password</b></p>
+            <center><button type="button" class="mt-4 text-center block bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600" onclick="fillDefaultAdmin()">Fill Default Admin Credential</button></center>
+        </div>
     </form>
+
+    <script>
+        function fillDefaultAdmin() {
+            document.getElementById('email').value = 'admin@admin.com';
+            document.getElementById('password').value = 'password';
+        }
+    </script>
 </x-guest-layout>
